e561cbb84c1af4d3e33cfa39452897bbec405a57
Florian ERNST <florianernst59@gmail.com>
2018-12-20 05:36:33 +0100

    Trying a service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+importScripts('/CrosswordGenerator/cache-polyfill.js');
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('main').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/dist/app.bundle.js',
+                '/index.html',
+                '/dist/camera.svg',
+                '/dist/trash.svg',
+            ]);
+        }).catch(error => {
+            console.error('Error caught |', error);
+        }),
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        }),
+    );
+});
